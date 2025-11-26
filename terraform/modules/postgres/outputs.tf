635341763cc486c728adfbea959d31a6d92bbea6
Paulo Toro <egosumscientia@gmail.com>
2025-11-26 13:48:55 -0500

    corrections

--- terraform/modules/postgres/outputs.tf
+++ terraform/modules/postgres/outputs.tf
@@ -2,10 +2,6 @@ output "postgres_hostname" {
   value = azurerm_postgresql_flexible_server.postgres.fqdn
 }
 
-output "private_endpoint_ip" {
-  value = azurerm_private_endpoint.postgres_pe.private_service_connection[0].private_ip_address
-}
-
 output "postgres_id" {
   value = azurerm_postgresql_flexible_server.postgres.id
 }
